a89b5b22afd48afefad630a7cac608b259f0f223
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2013-09-19 13:26:27 +0000

    bootstrapper: exlude hidden fields

--- classes/Bootstrapper.php
+++ classes/Bootstrapper.php
@@ -4,9 +4,12 @@ namespace HeimrichHannot;
 
 class Bootstrapper extends \Controller
 {
+	protected static $arrSkipTypes = array('hidden');
 
 	public static function generateForm(\Widget $objWidget)
 	{
+		if(in_array($objWidget->type, static::$arrSkipTypes)) return $objWidget->generate();
+
 		$strTemplate = 'bootstrapper_form';
 
 		$objT = new \FrontendTemplate($strTemplate);
