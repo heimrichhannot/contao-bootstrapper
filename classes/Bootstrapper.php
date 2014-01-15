7c32f8d89f8d5cdf86e1e877a905d7a8cfdec930
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2014-01-15 17:18:04 +0100

    php dateformat to js dateformat fix

--- classes/Bootstrapper.php
+++ classes/Bootstrapper.php
@@ -30,12 +30,69 @@ class Bootstrapper extends \Controller
 
 		return static::generateForm($objWidget, empty($objWidget->label));
 	}
-
-	public static function formatPhpDateToJsDate($format)
-	{
-		$PHPFormatOptions = array('y', 'Y', 'i', 'd', 'm');
-		$JSFormatOptions = array('YY', 'YYYY', 'mm', 'DD', 'MM');
-		$JSFormat = str_replace($PHPFormatOptions, $JSFormatOptions, $format);
-		return $JSFormat;
+	
+	public static function formatPhpDateToJsDate($php_format) {
+		$SYMBOLS_MATCHING = array (
+				// Day
+				'd' => 'DD',
+				'D' => 'D',
+				'j' => 'd',
+				'l' => 'DD',
+				'N' => '',
+				'S' => '',
+				'w' => '',
+				'z' => 'o',
+				// Week
+				'W' => '',
+				// Month
+				'F' => 'MM',
+				'm' => 'MM',
+				'M' => 'M',
+				'n' => 'm',
+				't' => '',
+				// Year
+				'L' => '',
+				'o' => '',
+				'Y' => 'YYYY',
+				'y' => 'y',
+				// Time
+				'a' => '',
+				'A' => '',
+				'B' => '',
+				'g' => '',
+				'G' => '',
+				'h' => '',
+				'H' => 'HH',
+				'i' => 'mm',
+				's' => '',
+				'u' => '' 
+		);
+		
+		$jqueryui_format = "";
+		$escaping = false;
+		
+		for($i = 0; $i < strlen ( $php_format ); $i ++)
+		{
+			$char = $php_format [$i];
+			if ($char === '\\') 			// PHP date format escaping character
+			{
+				$i ++;
+				if ($escaping)
+					$jqueryui_format .= $php_format [$i];
+				else
+					$jqueryui_format .= '\'' . $php_format [$i];
+				$escaping = true;
+			} else {
+				if ($escaping) {
+					$jqueryui_format .= "'";
+					$escaping = false;
+				}
+				if (isset ( $SYMBOLS_MATCHING [$char] ))
+					$jqueryui_format .= $SYMBOLS_MATCHING [$char];
+				else
+					$jqueryui_format .= $char;
+			}
+		}
+		return $jqueryui_format;
 	}
 }
