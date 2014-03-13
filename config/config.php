099a1bb3ff2ef709051fae0f91c386490a3a6c4d
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2014-03-13 11:20:47 +0100

    bootstrap css array fix

--- config/config.php
+++ config/config.php
@@ -4,4 +4,4 @@
 $GLOBALS['TL_HOOKS']['parseWidget'][] = array('Bootstrapper', 'parseWidgetHook');
 
 $GLOBALS['TL_USER_CSS']['form-bootstrap'] = 'system/modules/bootstrapper/assets/css/form.less|screen|static|3.1.1';
-$GLOBALS['TL_USER_CSS']['form-bootstrap'] = 'system/modules/bootstrapper/assets/css/pagination.less|screen|static|3.1.1';
+$GLOBALS['TL_USER_CSS']['pagination-bootstrap'] = 'system/modules/bootstrapper/assets/css/pagination.less|screen|static|3.1.1';
