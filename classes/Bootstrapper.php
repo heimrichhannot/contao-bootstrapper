6f7d9e8a21213facc610497d0979bef6322eb4da
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2014-04-24 16:40:24 +0200

    date & timepicker displayed regarding field rgxp

--- classes/Bootstrapper.php
+++ classes/Bootstrapper.php
@@ -4,7 +4,7 @@ namespace HeimrichHannot;
 
 class Bootstrapper extends \Controller
 {
-	protected static $arrSkipTypes = array('hidden', 'fieldset');
+	protected static $arrSkipTypes = array('hidden', 'fieldset', 'explanation');
 
 	public static function generateForm(\Widget $objWidget, $hideLabel=false)
 	{
