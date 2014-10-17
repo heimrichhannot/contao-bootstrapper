801252b8f692ea978d8899f6b017a708d9bfc872
Kevin Herklotz <k.herklotz@heimrich-hannot.de>
2014-10-17 14:53:45 +0200

    elastislide deleted

--- config/autoload.php
+++ config/autoload.php
@@ -25,15 +25,15 @@ ClassLoader::addNamespaces(array
  */
 ClassLoader::addClasses(array
 (
-	// Forms
-	'HeimrichHannot\FormButtonSubmit'  => 'system/modules/bootstrapper/forms/FormButtonSubmit.php',
+	// Classes
+	'HeimrichHannot\BootstrapperHooks' => 'system/modules/bootstrapper/classes/BootstrapperHooks.php',
+	'HeimrichHannot\Bootstrapper'      => 'system/modules/bootstrapper/classes/Bootstrapper.php',
 
 	// Modules
 	'HeimrichHannot\ModuleNewsReader'  => 'system/modules/bootstrapper/modules/news/ModuleNewsReader.php',
 
-	// Classes
-	'HeimrichHannot\BootstrapperHooks' => 'system/modules/bootstrapper/classes/BootstrapperHooks.php',
-	'HeimrichHannot\Bootstrapper'      => 'system/modules/bootstrapper/classes/Bootstrapper.php',
+	// Forms
+	'HeimrichHannot\FormButtonSubmit'  => 'system/modules/bootstrapper/forms/FormButtonSubmit.php',
 ));
 
 
@@ -42,24 +42,24 @@ ClassLoader::addClasses(array
  */
 TemplateLoader::addFiles(array
 (
-	'nav_navbar'                => 'system/modules/bootstrapper/templates/navigation',
-	'nav_list_unstyles'         => 'system/modules/bootstrapper/templates/navigation',
-	'nav_navbar_collapse'       => 'system/modules/bootstrapper/templates/navigation',
-	'nav_navbar_collapse_hover' => 'system/modules/bootstrapper/templates/navigation',
-	'pagination'                => 'system/modules/bootstrapper/templates/pagination',
-	'fe_page'                   => 'system/modules/bootstrapper/templates/frontend',
-	'fe_page_styleguide'        => 'system/modules/bootstrapper/templates/frontend',
+	'bootstrapper_form'         => 'system/modules/bootstrapper/templates/widgets',
 	'j_bootstrapgallery'        => 'system/modules/bootstrapper/templates/jquery',
-	'news_full_modal'           => 'system/modules/bootstrapper/templates/news',
-	'news_full_modal_content'   => 'system/modules/bootstrapper/templates/news',
+	'mod_search_advanced'       => 'system/modules/bootstrapper/templates/modules',
+	'mod_newsreader_modal'      => 'system/modules/bootstrapper/templates/modules/news',
 	'mod_search_simple'         => 'system/modules/bootstrapper/templates/modules',
 	'mod_password'              => 'system/modules/bootstrapper/templates/modules',
-	'mod_newsreader_modal'      => 'system/modules/bootstrapper/templates/modules/news',
-	'mod_search_advanced'       => 'system/modules/bootstrapper/templates/modules',
+	'news_full_modal'           => 'system/modules/bootstrapper/templates/news',
+	'news_full_modal_content'   => 'system/modules/bootstrapper/templates/news',
+	'pagination'                => 'system/modules/bootstrapper/templates/pagination',
+	'ce_accordion_stop'         => 'system/modules/bootstrapper/templates/elements',
 	'ce_accordion_start'        => 'system/modules/bootstrapper/templates/elements',
 	'ce_hyperlink'              => 'system/modules/bootstrapper/templates/elements',
-	'ce_accordion_stop'         => 'system/modules/bootstrapper/templates/elements',
-	'styleguide_bs3'            => 'system/modules/bootstrapper/templates/styleguides',
-	'bootstrapper_form'         => 'system/modules/bootstrapper/templates/widgets',
 	'block_button'              => 'system/modules/bootstrapper/templates/block',
+	'fe_page'                   => 'system/modules/bootstrapper/templates/frontend',
+	'fe_page_styleguide'        => 'system/modules/bootstrapper/templates/frontend',
+	'nav_navbar_collapse_hover' => 'system/modules/bootstrapper/templates/navigation',
+	'nav_navbar_collapse'       => 'system/modules/bootstrapper/templates/navigation',
+	'nav_navbar'                => 'system/modules/bootstrapper/templates/navigation',
+	'nav_list_unstyles'         => 'system/modules/bootstrapper/templates/navigation',
+	'styleguide_bs3'            => 'system/modules/bootstrapper/templates/styleguides',
 ));
