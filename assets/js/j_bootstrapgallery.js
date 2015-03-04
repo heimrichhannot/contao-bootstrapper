88a5b07d9a8df8b7840504c25fd940dc45d14fe1
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-03-04 17:47:27 +0100

    slick slider blueimp support (skip cloned items in gallery)

--- assets/js/j_bootstrapgallery.js
+++ assets/js/j_bootstrapgallery.js
@@ -8,10 +8,27 @@
 				var $this = $(this),
 					galleryId = $this.data('lightbox');
 
+                // skip slick slider, done after init callback
+                if($this.parents('.slick').length > 0) return true;
+
 				if(!$this.data('gallery') && galleryId ){
 					$this.attr('data-gallery', '#blueimp-gallery-' + galleryId);
 				}
 			});
+
+            $('.slick').on('init', function(){
+
+                // blueimp support for non-cloned slick items
+                $(this).find('.slick-slide:not(.slick-cloned) a[data-lightbox]').each(function(){
+                    var $this = $(this),
+                        galleryId = $this.data('lightbox');
+
+                    if(!$this.data('gallery') && galleryId ){
+                        $this.attr('data-gallery', '#blueimp-gallery-' + galleryId);
+                    }
+                });
+
+            });
 	});
 	
 })(jQuery);
