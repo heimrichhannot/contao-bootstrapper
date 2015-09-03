d5f979e9cce4c6e318afeb76f0cd737cd81059fc
Defcon0 <patzer.dennis@googlemail.com>
2015-09-03 16:10:11 +0200

    added multiColumnWizard to skipped

--- classes/Bootstrapper.php
+++ classes/Bootstrapper.php
@@ -4,7 +4,7 @@ namespace HeimrichHannot;
 
 class Bootstrapper extends \Controller
 {
-	protected static $arrSkipTypes = array('hidden', 'fieldset', 'explanation', 'subEventList', 'successGroup', 'formcolstart', 'formcolpart', 'formcolend');
+	protected static $arrSkipTypes = array('hidden', 'fieldset', 'explanation', 'subEventList', 'successGroup', 'formcolstart', 'formcolpart', 'formcolend', 'multiColumnWizard');
 
 	public static function generateForm(\Widget $objWidget, $hideLabel=false)
 	{
