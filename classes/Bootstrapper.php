792218853310102450139d786ee95b61a4c6401a
Jenny Reinhardt <j.reinhardt@heimrich-hannot.de>
2017-07-07 10:42:54 +0200

    multifileupload

--- classes/Bootstrapper.php
+++ classes/Bootstrapper.php
@@ -6,7 +6,7 @@ use HeimrichHannot\Bootstrapper\BootstrapperFormField;
 
 class Bootstrapper extends \Controller
 {
-	protected static $arrSkipTypes = array('hidden', 'fieldset', 'subEventList', 'explanation', 'successGroup', 'formcolstart', 'formcolpart', 'formcolend', 'multiColumnWizard', 'form_multiColumnWizard');
+	protected static $arrSkipTypes = array('hidden', 'fieldset', 'subEventList', 'explanation', 'successGroup', 'formcolstart', 'formcolpart', 'formcolend', 'multiColumnWizard', 'form_multiColumnWizard', 'multifileupload');
 
 	public static function generateForm(\Widget $objWidget, $hideLabel=false)
 	{
